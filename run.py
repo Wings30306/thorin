1f1ed8cdc536a349af5db8d80ef32c1db5f80576
Joke Heyndels <jo_hannah@outlook.com>
2018-08-19 11:30:30 +0200

    added member pages

--- run.py
+++ run.py
@@ -1,27 +1,48 @@
 import os
+import json
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-  return render_template("index.html", page_title="Thorin and Company")
+    return render_template("index.html", page_title="Thorin and Company")
+
 
 @app.route('/about')
 def about():
-  return render_template("about.html", page_title="About the Company")
+    data = []
+    with open("data/company.json", "r", encoding='utf-8') as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About the Company", company_data=data)
+
+
+@app.route('/about/<member_name>')
+def about_member(member_name):
+    member = {}
+
+    with open("data/company.json", "r", encoding='utf-8') as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+
+    return render_template("member.html", member=member)
+
 
 @app.route('/contact')
 def contact():
-  return render_template("contact.html", page_title="Contact us")
+    return render_template("contact.html", page_title="Contact us")
+
 
 @app.route("/careers")
 def careers():
-  return render_template("careers.html", page_title="Join our Company!")
+    return render_template("careers.html", page_title="Join our Company!")
+
 
 if __name__ == "__main__":
-  app.run(host=os.getenv("IP"),
-          port=os.getenv("PORT"),
-          debug=True)
-
+    app.run(host=os.getenv("IP"),
+            port=os.getenv("PORT"),
+            debug=True)
